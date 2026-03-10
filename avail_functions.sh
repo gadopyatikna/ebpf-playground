c62dd2bbaedd0396bd33ea0cc0b85edfe90370c6
alex <5575343+gadopyatikna@users.noreply.github.com>
2026-03-10 16:07:53 +0100

    Enhance avail_functions.sh with user space probes
    Add user space probing commands for Python functions.

--- avail_functions.sh
+++ avail_functions.sh
@@ -1,3 +1,7 @@
 sudo cat /sys/kernel/debug/tracing/available_filter_functions | grep accept
 sudo cat /sys/kernel/debug/tracing/available_filter_functions | grep epoll
 sudo ls /sys/kernel/debug/tracing/events/syscalls | grep epoll
+
+# user space
+perf probe -x /usr/bin/python3 --funcs
+bpftrace -l 'uprobe:/usr/bin/python3:*'
